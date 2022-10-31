01463f690b36e494b4b3f9996b899dc9ffee5023
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2022-10-31 21:16:21 +0300

    Создание окна и его размеры

--- src/Formula.java
+++ src/Formula.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Formula {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Formula extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 400;
+
+    private JTextField textFieldX;
+    private JTextField textFieldY;
+    private JTextField textFieldZ;
+
+
+    private JTextField textFieldResult;
+
+    private ButtonGroup radioButtons = new ButtonGroup();
+
+    private Box hboxFormulaType = Box.createHorizontalBox();
+
+    private int formulaId = 1;
+
+    public double Form1(double x, double y, double z) {
+        return 1; // вернем значения своего варианта
+    }
+    public double Form2(double x, double y, double z) {
+        return 2; // так же свой вариант
+    }
+    // Вспомогательный метод для добавления кнопок на панель
+    private void addRadioButton(String buttonName, final int formulaId) {
+        JRadioButton button = new JRadioButton(buttonName);
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                Formula.this.formulaId = formulaId;
+            }
+        });
+        radioButtons.add(button);
+        hboxFormulaType.add(button);
+    }
+    public Formula()
+    {
+        super("Вычисление формулы");
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        setLocation((kit.getScreenSize().width - WIDTH)/2,// это чтобы экран был по центру, без этого оно будет в
+                (kit.getScreenSize().height - HEIGHT)/2); // левом верхнем углу
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         // пишу оконное приложение для второй Лабы
-        JFrame frame = new JFrame();
+        Formula frame = new Formula();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
