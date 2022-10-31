f4377c3738b1093627d18d521083c4abe3ac1a10
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2022-10-31 20:35:32 +0300

    Создание фрейма

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // пишу оконное приложение для второй Лабы
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
