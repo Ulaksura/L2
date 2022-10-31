33030901d1520eccc43d2cb59b04e72a31ee0062
Kseniya Ulasovets <kseniyaulasovets@gmail.com>
2022-10-31 21:49:35 +0300

    Итог

--- src/Formula.java
+++ src/Formula.java
@@ -21,11 +21,14 @@ public class Formula extends JFrame {
     private int formulaId = 1;
 
     public double Form1(double x, double y, double z) {
-        return 1; // вернем значения своего варианта
+        return Math.sin(Math.log(y)+Math.sin(3.14*y*y))*Math.pow(x*x+Math.sin(z)+Math.exp(Math.cos(z)),1/4);
     }
+
     public double Form2(double x, double y, double z) {
-        return 2; // так же свой вариант
+        return Math.pow(Math.cos(Math.exp(x))+Math.log((1+y)*(1+y))+Math.sqrt(Math.exp(Math.cos(x))+Math.sin(3.14*z)*Math.sin(3.14*z))+
+                Math.sqrt(1/x)+Math.cos(y*y),Math.sin(z));
     }
+
     // Вспомогательный метод для добавления кнопок на панель
     private void addRadioButton(String buttonName, final int formulaId) {
         JRadioButton button = new JRadioButton(buttonName);
@@ -38,14 +41,94 @@ public class Formula extends JFrame {
         radioButtons.add(button);
         hboxFormulaType.add(button);
     }
-    public Formula()
-    {
+
+    public Formula() {
         super("Вычисление формулы");
         setSize(WIDTH, HEIGHT);
         Toolkit kit = Toolkit.getDefaultToolkit();
-        setLocation((kit.getScreenSize().width - WIDTH)/2,// это чтобы экран был по центру, без этого оно будет в
-                (kit.getScreenSize().height - HEIGHT)/2); // левом верхнем углу
+        setLocation((kit.getScreenSize().width - WIDTH) / 2,// это чтобы экран был по центру, без этого оно будет в
+                (kit.getScreenSize().height - HEIGHT) / 2); // левом верхнем углу
 
+        addRadioButton("Формула 1", 1); // добавление радиокнопок под наши формулы
+        addRadioButton("Формула 2", 2);
+
+        radioButtons.setSelected(radioButtons.getElements().nextElement().getModel(), true);
+
+        JLabel labelForX = new JLabel("X:");
+        textFieldX = new JTextField("0", 10);
+        textFieldX.setMaximumSize(textFieldX.getPreferredSize());
+        JLabel labelForY = new JLabel("Y:");
+        textFieldY = new JTextField("0", 10);       // отводим место для полей ввода значений
+        textFieldY.setMaximumSize(textFieldY.getPreferredSize());
+        JLabel labelForZ = new JLabel("Z:");
+        textFieldZ = new JTextField("0", 10);
+        textFieldZ.setMaximumSize(textFieldZ.getPreferredSize());
+
+        Box hboxVariables = Box.createHorizontalBox();
+        hboxVariables.add(labelForX);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldX);
+        hboxVariables.add(Box.createHorizontalStrut(40));
+        hboxVariables.add(labelForY);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldY);
+        hboxVariables.add(Box.createHorizontalStrut(40));
+        hboxVariables.add(labelForZ);
+        hboxVariables.add(Box.createHorizontalStrut(10));
+        hboxVariables.add(textFieldZ);
+
+        JLabel labelForResult = new JLabel("Результат:");
+        textFieldResult = new JTextField("0", 10);
+        Box hboxResult = Box.createHorizontalBox();
+        hboxResult.add(labelForResult);
+        hboxResult.add(Box.createHorizontalStrut(10));
+        hboxResult.add(labelForResult);
+        hboxResult.add(Box.createHorizontalGlue());
+
+        JButton buttonCalc = new JButton("Вычислить");
+        buttonCalc.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                Double x = Double.parseDouble(textFieldX.getText());
+                Double y = Double.parseDouble(textFieldY.getText());
+                Double z = Double.parseDouble(textFieldZ.getText());
+                Double result;
+
+                if (formulaId == 1)
+                    result = Form1(x, y, z);
+                else
+                    result = Form2(x, y, z);
+                labelForResult.setText(result.toString());
+            }
+
+            ;
+        });
+        JButton buttonReset = new JButton("Очистить поля");
+        buttonReset.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                textFieldX.setText("0");
+                textFieldY.setText("0");
+                textFieldZ.setText("0");
+                textFieldResult.setText("0");
+
+            }
+        });
+        Box hboxButtons = Box.createHorizontalBox();
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.add(buttonCalc);
+        hboxButtons.add(Box.createHorizontalStrut(30));
+        hboxButtons.add(buttonReset);
+        hboxButtons.add(Box.createHorizontalGlue());
+        hboxButtons.setBorder(
+                BorderFactory.createLineBorder(Color.GREEN));
+// Связать области воедино в компоновке BoxLayout
+
+        Box contentBox = Box.createVerticalBox();
+        contentBox.add(Box.createVerticalGlue());
+        contentBox.add(hboxFormulaType);
+        contentBox.add(hboxVariables);
+        contentBox.add(hboxResult);
+        contentBox.add(hboxButtons);
+        contentBox.add(Box.createVerticalGlue());
+        getContentPane().add(contentBox, BorderLayout.CENTER);
     }
-
 }
